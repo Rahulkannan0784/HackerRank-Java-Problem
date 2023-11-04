1cb449a5f328264d54dd430eb16a528713d59fd2
Rahulkannan <115389026+Rahulkannan0784@users.noreply.github.com>
2023-11-04 10:08:14 +0530

    easy

---
+++ Java End-of-file/code.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+
+   for(int i =1;sc.hasNext();i++){
+       String s = sc.nextLine();
+       System.out.println(i + " " + s);                
+
+   }
+    }
+       
+    
+}
